83951c396caa56649ca02c84c1c10991fcd56f4d
DylanL24 <dylan.lee@student.tamdistrict.org>
2024-10-21 12:01:45 -0700

    created file and Main method

---
+++ ChooseYourPath.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ChooseYourPath
+{
+    public static void Main(String[] args) 
+    {
+        System.out.println("First and 10 at the 30. What play do you run? a for pass or b for run")
+    }
+} 
